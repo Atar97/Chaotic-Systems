2b7aee2f67028def39bf47b134016f7d3b5e85fd
Austin <austin.cotant@gmail.com>
2018-10-29 11:15:41 -0700

    Add package.json and start vectors

---
+++ frontend/physics/vector.js
@@ -0,0 +1,14 @@
+class Vector {
+    constructor(options) {
+        const {dimensions, vectors} = options;
+        this.components = []
+        if (vectors) {
+            this.components = Array.from(vectors)
+        } else {
+            dimensions = dimensions || 1
+            for (let idx = 0; idx < dimensions; idx++) {
+                this.components.push(0)
+            }
+        }
+    }
+}
