3d9cb7999f29b337ac4ae5d02c9e454dcb0e8722
Austin <austin.cotant@gmail.com>
2018-10-30 15:52:26 -0700

    Create canvas

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,1 +1,12 @@
 import Test from './physics/test';
+import { createGzip } from 'zlib';
+
+document.addEventListener('DOMContentLoaded', () => {
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = "red";
+    ctx.fillRect(100, 100, 400, 300); 
+    ctx.beginPath();
+    ctx.arc(100, 200, 50, 0, 2*Math.PI);
+    ctx.stroke();
+})
